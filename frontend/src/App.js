35e5cd308594986a2082631f9c8a3456547e1617
Tharukshi Diyunugala <139604971+TharukshiDiyunugala@users.noreply.github.com>
2024-12-20 01:38:14 +0530

    Basic modifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 // import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from 'E:/AI-Integrated-E-Commerce-Platform/frontend/src/Components/Homepage';
-import ProductPage from 'E:/AI-Integrated-E-Commerce-Platform/frontend/src/Components/ProductPage';
-import CartPage from 'E:/AI-Integrated-E-Commerce-Platform/frontend/src/Components/CartPage.js';
-import CheckoutPage from 'E:/AI-Integrated-E-Commerce-Platform/frontend/src/Components/CheckOutPage';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Homepage from 'E:/AI-Integrated-E-Commerce-Platform/frontend/src/Components/Homepage';
+// import ProductPage from 'E:/AI-Integrated-E-Commerce-Platform/frontend/src/Components/ProductPage';
+// import CartPage from 'E:/AI-Integrated-E-Commerce-Platform/frontend/src/Components/CartPage.js';
+// import CheckoutPage from 'E:/AI-Integrated-E-Commerce-Platform/frontend/src/Components/CheckOutPage';
+import Navbar from './Components/navbar/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/product" element={<ProductPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <Navbar/>
+    </div>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Homepage />} />
+    //     <Route path="/product" element={<ProductPage />} />
+    //     <Route path="/cart" element={<CartPage />} />
+    //     <Route path="/checkout" element={<CheckoutPage />} />
+    //   </Routes>
+    // </Router>
   );
 }
 
--- frontend/src/Components/Homepage.js
+++ frontend/src/Components/Homepage.js
@@ -12,3 +12,5 @@ function Homepage() {
 }
 
 export default Homepage;
+
+
